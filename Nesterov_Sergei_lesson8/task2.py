515378c6407082df0659fbd8f0970edfb0d88ddd
SerJ <ury_047@inbox.ru>
2022-07-14 21:31:19 +0300

    Python_lesson8

---
+++ Nesterov_Sergei_lesson8/task2.py
@@ -0,0 +1,21 @@
+# Создайте собственный класс-исключение, обрабатывающий ситуацию деления на ноль.
+# Проверьте его работу на данных, вводимых пользователем.
+# При вводе нуля в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
+
+
+class DivisionByNull:
+    def __init__(self, divider, denominator):
+        self.divider = divider
+        self.denominator = denominator
+
+    @staticmethod
+    def divide_by_null(divider, denominator):
+        try:
+            return (divider / denominator)
+        except:
+            return (f"Делить на ноль нельзя!!!")
+
+
+div = DivisionByNull(4, 80)
+print(DivisionByNull.divide_by_null(4, 2))
+print(div.divide_by_null(80, 0))
